e6ef46950d7937a1820201db018c23fbed41dd80
catherine-256 <148316909+catherine-256@users.noreply.github.com>
2023-10-22 19:12:23 +0100

    week 2

---
+++ Week 2/practice2.rs
@@ -0,0 +1,6 @@
+Fn main(){
+println!(); //prints just a 
+println!("hello");//prints
+println!("format {} arguments" , "some"); //prints format some arguments 
+println!("My name is {}. I am the class of stream 1", "Catherine Okoede");
+}
